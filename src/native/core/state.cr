429950cce8d7a52c9a24d0ab112d177f16860bd3
ZenDrx <ynwghosted@icloud.com>
2026-06-05 16:47:23 +0100

    Delete pretty method from state.cr
    Removed the pretty method for JSON serialization.

--- src/native/core/state.cr
+++ src/native/core/state.cr
@@ -32,13 +32,5 @@ module Native::Core
     rescue
       false
     end
-
-    def self.pretty(obj : JSON::Serializable) : String
-      io = IO::Memory.new
-      JSON::PrettyWriter.new(io).write(obj)
-      io.to_s
-    rescue ex : Exception
-      raise SerializationError.new("Failed to pretty print state: #{ex.message}")
-    end
   end
 end
