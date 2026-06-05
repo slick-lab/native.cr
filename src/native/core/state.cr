82b9f7410a6477077026400f229897ca3a6c188c
ZenDrx <ynwghosted@icloud.com>
2026-06-05 19:01:10 +0100

    Refactor load method to merge JSON into default instance

--- src/native/core/state.cr
+++ src/native/core/state.cr
@@ -13,12 +13,22 @@ module Native::Core
     rescue ex : Exception
       raise SerializationError.new("Failed to save state: #{ex.message}")
     end
-
-    def self.load(json : String, klass : JSON::Serializable.class) : JSON::Serializable
-      klass.from_json(json)
-    rescue ex : Exception
-      raise DeserializationError.new("Failed to load state: #{ex.message}")
-    end
+def self.load(json : String, klass : JSON::Serializable.class) : JSON::Serializable
+  data = JSON.parse(json)
+  
+  # Create default instance first
+  default = klass.new
+  
+  # Merge JSON into default
+  merged = default.to_json
+  parsed_merged = JSON.parse(merged)
+  
+  data.each do |key, value|
+    parsed_merged[key] = value
+  end
+  
+  klass.from_json(parsed_merged.to_json)
+end
 
     def self.capture_and_restore(obj : JSON::Serializable, &block) : JSON::Serializable
       saved_state = save(obj)
