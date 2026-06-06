d563715457ba34915dd90f17e972c6e65cb5b5a6
ZenDrx <ynwghosted@icloud.com>
2026-06-07 00:12:31 +0100

    Fix file path comment in android_entry.cr

--- src/native/engine/android/android_entry.cr
+++ src/native/engine/android/android_entry.cr
@@ -1,6 +1,5 @@
-# src/native/android_entry.cr
+# src/native/# src/native/engine/android/android_entry.cr
 
-@[Export("crystal_android_main")]
 fun crystal_android_main(state : Void*) : Void
   GC.init
   app = NativeApp.current
