6e1fecb720d198d5364d50550150b5dd498ee8ff
ZenDrx <ynwghosted@icloud.com>
2026-06-08 18:01:03 +0100

    Refactor iOS bridge functions for Crystal integration

--- src/native/engine/ios/bridge.cr
+++ src/native/engine/ios/bridge.cr
@@ -1,37 +1,37 @@
 # src/native/engine/ios/bridge.cr
 
-@[Link("objc")]
-@[Link("Foundation")]
-@[Link("UIKit")]
-@[Link("Metal")]
-@[Link("QuartzCore")]
-lib LibIOS
-  fun UIApplicationMain(argc: Int32, argv: UInt8**, principalClassName: UInt8*, delegateClassName: UInt8*) : Int32
-end
-
-@[Export("ios_app_main")]
-fun ios_app_main : Void
+@[Export("crystal_init")]
+fun crystal_init : Void
   GC.init
-  Native::App.current.run
 end
 
-@[Export("ios_render_frame")]
-fun ios_render_frame : Void
-  if app = Native::App.current?
-    app.draw
-  end
+@[Export("crystal_start")]
+fun crystal_start : Void
+  app = Native::App.current
+  app.load_saved_state
+  app.setup
 end
 
-@[Export("ios_handle_touch")]
-fun ios_handle_touch(x : Float32, y : Float32, action : Int32) : Void
-  if app = Native::App.current?
-    case action
-    when 0
-      app.on_touch_began(x, y)
-    when 1
-      app.on_touch_moved(x, y)
-    when 2
-      app.on_touch_ended(x, y)
-    end
-  end
+@[Export("crystal_render_frame")]
+fun crystal_render_frame : Void
+  app = Native::App.current
+  app.draw
+end
+
+@[Export("crystal_touch_began")]
+fun crystal_touch_began(x : Float32, y : Float32) : Void
+  app = Native::App.current
+  app.on_touch_began(x, y)
+end
+
+@[Export("crystal_touch_moved")]
+fun crystal_touch_moved(x : Float32, y : Float32) : Void
+  app = Native::App.current
+  app.on_touch_moved(x, y)
+end
+
+@[Export("crystal_touch_ended")]
+fun crystal_touch_ended(x : Float32, y : Float32) : Void
+  app = Native::App.current
+  app.on_touch_ended(x, y)
 end
