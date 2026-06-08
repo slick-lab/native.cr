a9247f75d593b7706ea0fd90cfb0cc669346a071
ZenDrx <ynwghosted@icloud.com>
2026-06-08 17:56:22 +0100

    Rename AppDelegate.m to AppDelegate.swift

---
+++ src/native/engine/ios/AppDelegate.swift
@@ -0,0 +1,19 @@
+// src/native/engine/ios/AppDelegate.swift
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        crystal_init()
+        crystal_start()
+        
+        return true
+    }
+}
