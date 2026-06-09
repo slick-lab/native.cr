7c7b5d971731e79519d0c4530f1fa31b7d5faff7
ZenDrx <ynwghosted@icloud.com>
2026-06-09 09:09:55 +0100

    Delete src/native/engine/android/android_entry.cr

--- src/native/engine/android/android_entry.cr
+++
@@ -1,8 +0,0 @@
-# src/native/# src/native/engine/android/android_entry.cr
-
-fun crystal_android_main(state : Void*) : Void
-  GC.init
-  app = Native::App.current
-  app.renderer = state
-  app.run
-end
