b5a3f9c18a8452f2e62803b3cf007bb3392c9726
ZenDrx <ynwghosted@icloud.com>
2026-06-24 07:32:46 +0100

    improved c code

--- src/native/engine/android/native.c
+++ src/native/engine/android/native.c
@@ -1,69 +1,108 @@
 // src/native/engine/android/native.c
 
-#include <jni.h>
-#include <android_native_app_glue.h>
+#include <android/native_activity.h>
 #include <android/log.h>
+#include <pthread.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native.cr", __VA_ARGS__))
 #define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "native.cr", __VA_ARGS__))
 
-static struct android_app* g_app = NULL;
+static ANativeActivity* g_activity = NULL;
 static JNIEnv* g_env = NULL;
-static jobject g_activity = NULL;
+static jobject g_activity_obj = NULL;
 static jclass g_activity_class = NULL;
 static JavaVM* g_vm = NULL;
 
 jobject get_activity() {
-  return g_activity;
+    return g_activity_obj;
 }
 
 JNIEnv* get_jni_env() {
-  return g_env;
+    return g_env;
 }
 
 jclass get_activity_class() {
-  return g_activity_class;
+    return g_activity_class;
 }
 
-void android_main(struct android_app* state) {
-  g_app = state;
-  g_vm = state->activity->vm;
-  
-  if ((*g_vm)->AttachCurrentThread(g_vm, &g_env, NULL) != JNI_OK) {
-    LOGE("Failed to attach thread to JVM");
-    return;
-  }
-  
-  g_activity = state->activity->clazz;
-  g_activity_class = (*g_env)->GetObjectClass(g_env, g_activity);
-  
-  if (g_activity_class == NULL) {
-    LOGE("Failed to get activity class");
-    return;
-  }
-  
-  LOGI("Android engine ready, JNI attached");
-  
-  extern void crystal_android_main(JNIEnv* env, jobject activity, jclass activity_class);
-  crystal_android_main(g_env, g_activity, g_activity_class);
-  
-  while (1) {
-    int ident;
-    int events;
-    struct android_poll_source* source;
-    
-    while ((ident = ALooper_pollOnce(0, NULL, &events, (void**)&source)) >= 0) {
-      if (source != NULL) {
-        source->process(state, source);
-      }
-      
-      if (state->destroyRequested != 0) {
-        (*g_vm)->DetachCurrentThread(g_vm);
-        LOGI("Android engine shutting down");
-        return;
-      }
+// Forward declaration
+extern void crystal_android_main(JNIEnv* env, jobject activity, jclass activity_class);
+
+// Background thread that runs Crystal code
+static void* crystal_thread(void* arg) {
+    ANativeActivity* activity = (ANativeActivity*)arg;
+
+    // Attach thread to JVM
+    if ((*g_vm)->AttachCurrentThread(g_vm, &g_env, NULL) != JNI_OK) {
+        LOGE("Failed to attach thread to JVM");
+        return NULL;
     }
-  }
+
+    // Get activity object and class
+    g_activity_obj = activity->clazz;
+    g_activity_class = (*g_env)->GetObjectClass(g_env, g_activity_obj);
+
+    if (g_activity_class == NULL) {
+        LOGE("Failed to get activity class");
+        (*g_vm)->DetachCurrentThread(g_vm);
+        return NULL;
+    }
+
+    LOGI("Android engine ready, calling Crystal main");
+
+    // Call into Crystal
+    crystal_android_main(g_env, g_activity_obj, g_activity_class);
+
+    LOGI("Crystal main returned");
+
+    // Detach thread
+    (*g_vm)->DetachCurrentThread(g_vm);
+    g_env = NULL;
+
+    return NULL;
+}
+
+// Lifecycle callbacks
+static void on_destroy(ANativeActivity* activity) {
+    LOGI("Activity destroyed");
+    // TODO: Signal Crystal to shutdown gracefully
+}
+
+static void on_pause(ANativeActivity* activity) {
+    LOGI("Activity paused");
+    // TODO: Signal Crystal to pause
+}
+
+static void on_resume(ANativeActivity* activity) {
+    LOGI("Activity resumed");
+    // TODO: Signal Crystal to resume
+}
+
+// Main entry point - called by NativeActivity
+void ANativeActivity_onCreate(ANativeActivity* activity, void* savedState, size_t savedStateSize) {
+    LOGI("ANativeActivity_onCreate called");
+
+    g_activity = activity;
+
+    // Get JavaVM from activity
+    JNIEnv* env = activity->env;
+    (*env)->GetJavaVM(env, &g_vm);
+
+    // Set up lifecycle callbacks
+    activity->callbacks->onDestroy = on_destroy;
+    activity->callbacks->onPause = on_pause;
+    activity->callbacks->onResume = on_resume;
+
+    // Start Crystal on background thread (don't block main thread)
+    pthread_t thread;
+    if (pthread_create(&thread, NULL, crystal_thread, activity) != 0) {
+        LOGE("Failed to create Crystal thread");
+        return;
+    }
+
+    // Detach thread so it cleans up automatically
+    pthread_detach(thread);
+
+    LOGI("Crystal thread started");
 }
